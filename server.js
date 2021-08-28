6d4c027b75e93c7f15161abcf17ae7f990bd786f
Fausnightm <m.fausnight@lafilm.edu>
2021-08-28 10:45:12 -0400

    "started handlebars file set up based on module"

--- server.js
+++ server.js
@@ -1,42 +1,60 @@
-const express = require("express");
-const path = require("path");
-//paths
-const controller = require("./controllers");
-//handlebars
-const exphbs = require("express-handlebars");
-//Sequelize
-const sequelize = require("./config/connection");
-//Session
-const session = require("express-session");
-const SequlizeStore = require("connect-session-sequelize")(session.Store);
+const path = require('path');
+// dotenv file for sensitive configuration information
+require('dotenv').config();
+// Express.js server
+const express = require('express');
+// All routes as defined in the controllers folder
+const routes = require('./controllers/');
+// Sequelize connection to the database
+const sequelize = require('./config/connection');
+// Handlebars template engine for front-end
+const exphbs = require('express-handlebars')
+// Express session to handle session cookies
+const session = require('express-session')
+// Sequelize store to save the session so the user can remain logged in
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// Handlebars helpers
+const helpers = require('./utils/helpers');
 
-//set up the actual session
+// Initialize handlebars for the html templates
+const hbs = exphbs.create({ helpers });
+
+// Initialize sessions
 const sess = {
-  secret: "super secret secret",
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequlizeStore({
-    db: sequelize,
-  }),
-};
-//initialize the server
+    secret: "super secret secret",
+    cookie: { maxAge: 7200000 },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
+// Initialize the server
 const app = express();
+// Define the port for the server
 const PORT = process.env.PORT || 3001;
 
-//middlewear
+// Give the server a path to the public directory for static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set handlebars as the template engine for the server
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Have Express parse JSON and string data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+
+// Tell the app to use Express Session for the session handling
 app.use(session(sess));
 
-//use controllers
-app.use("/", controller);
+// Give the server the path to the routes
+// app.use(routes);
 
-//set handlebars as render engine
-app.engine("handlebars", exphbs());
-app.set("view engine", "handlebars");
-
+// Turn on connection to db and then to the server
+// force: true to reset the database and clear all values, updating any new relationships
+// force: false to maintain data - aka normal operation
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
-});
+    app.listen(PORT, () => console.log('Now listening'));
+  });
